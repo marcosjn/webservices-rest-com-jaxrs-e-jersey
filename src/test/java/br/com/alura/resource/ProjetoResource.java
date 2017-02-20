4ee21e7cadfdc5c77be49dcd46c132609b2a7641
Marcos Nonnenmacher <marcosjn@gmail.com>
2017-02-19 21:08:50 -0300

    commitando aula 09

--- src/test/java/br/com/alura/resource/ProjetoResource.java
+++ src/test/java/br/com/alura/resource/ProjetoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,7 +14,9 @@ import javax.ws.rs.core.Response;
 
 import com.thoughtworks.xstream.XStream;
 
+import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.dao.ProjetoDAO;
+import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Projeto;
 
 @Path("projetos")
@@ -34,5 +37,12 @@ public class ProjetoResource {
 		URI uri = URI.create("/projetos/" + projeto.getId());
 		return Response.created(uri).build();		
 	}
+	
+	@Path("{id}")
+	@DELETE
+	public Response remove(@PathParam("id") long id) {
+		new ProjetoDAO().remove(id);
+		return Response.ok().build();
+	}		
 }
 	
