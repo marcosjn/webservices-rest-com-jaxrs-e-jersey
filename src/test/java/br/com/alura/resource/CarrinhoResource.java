005480c1e84ec0a61312dae79f7756451d8cc223
Marcos Nonnenmacher <marcosjn@trt4.jus.br>
2017-02-21 16:33:41 -0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/marcosjn/webservices-rest-com-jaxrs-e-jersey
    
    # Conflicts:
    #	src/main/java/br/com/alura/loja/dao/ProjetoDAO.java
    #	src/main/java/br/com/alura/loja/modelo/Projeto.java
    #	src/test/java/br/com/alura/loja/Servidor.java
    #	src/test/java/br/com/alura/resource/ProjetoResource.java

--- src/test/java/br/com/alura/resource/CarrinhoResource.java
+++ src/test/java/br/com/alura/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -23,10 +25,10 @@ public class CarrinhoResource {
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") long id) {
-	    Carrinho carrinho = new CarrinhoDAO().busca(id);
-	    return carrinho.toXML();
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		return carrinho.toXML();
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response adiciona(String conteudo) {
@@ -35,12 +37,22 @@ public class CarrinhoResource {
 		URI uri = URI.create("/carrinhos/" + carrinho.getId());
 		return Response.created(uri).build();
 	}
-	
+
 	@Path("{id}/produtos/{ProdutoId}")
 	@DELETE
 	public Response remove(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoId);
 		return Response.ok().build();
-	}	
+	}
+
+	@Path("{id}/produtos/{produtoId}/quantidade")
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.troca(produto);
+		return Response.ok().build();
+	}
 }
